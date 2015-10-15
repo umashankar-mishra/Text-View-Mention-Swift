9983605530371d17b362af2898559f87a04b3e1a
j3patel <jatinpatel101@gmail.com>
2015-10-15 19:46:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Text-View-Mention-Swift
---
+++ mention/JPAnnotatedTV.swift
@@ -0,0 +1,19 @@
+//
+//  JPAnnotatedTF.swift
+//  mention
+//
+//  Created by RajaSoftwareLabs on 15/10/15.
+//  Copyright © 2015 jatin. All rights reserved.
+//
+
+import UIKit
+
+class JPAnnotatedTF: UITextField {
+
+
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
